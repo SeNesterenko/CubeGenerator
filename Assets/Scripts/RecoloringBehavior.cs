dd3e4d2092b92c54d122e01c7687e79990cd87a7
snesterenko <snesterenko@dtln.ru>
2022-10-04 20:04:39 +0300

    Resizing logic

--- Assets/Scripts/RecoloringBehavior.cs
+++ Assets/Scripts/RecoloringBehavior.cs
@@ -3,16 +3,26 @@ using Random = UnityEngine.Random;
 
 public class RecoloringBehavior : MonoBehaviour
 {
-    [SerializeField] private float _recoloringDuration = 2f;
-    [SerializeField] private int _recoloringDetention;
+    [SerializeField] private float _detention;
+    
+    [SerializeField] private float _recoloringDuration;
 
-    private Color _startColor;
-    private Color _nextColor;
+    [SerializeField] private float _resizingDuration;
+    [SerializeField] private Vector3 _objectScale;
+
     private Renderer _renderer;
 
-    private float _currentTimeDuration;
-    private float _currentTimeDetention;
-    private bool _isWaitRecoloring = false;
+    private Vector3 _startSize = new (1, 1, 1);
+    private float _currentTimeDurationResizing;
+    private float _currentTimeDetentionResizing;
+    private bool _isWaitResizing;
+    
+    private Color _startColor;
+    private Color _nextColor;
+    private float _currentTimeDurationRecoloring;
+    private float _currentTimeDetentionRecoloring;
+    private bool _isWaitRecoloring;
+    
 
     private void Awake()
     {
@@ -22,30 +32,62 @@ public class RecoloringBehavior : MonoBehaviour
 
     private void Update()
     {
-        if (_isWaitRecoloring)
-        {
-            _currentTimeDetention += Time.deltaTime;
+        RecolorObject();
+        ResizingObject();
+    }
 
-            if (_currentTimeDetention >= _recoloringDetention)
-            {
-                _currentTimeDetention = 0f;
-                GenerateNextColor();
-                _isWaitRecoloring = !_isWaitRecoloring;
-            }
+    private void ResizingObject()
+    {
+        if (_isWaitResizing)
+        {
+            _currentTimeDetentionResizing += Time.deltaTime;
+
+            if (!(_currentTimeDetentionResizing >= _detention)) return;
+            _currentTimeDetentionResizing = 0f;
+            GenerateNextScale();
+            _isWaitResizing = !_isWaitResizing;
             return;
         }
-        
-        _currentTimeDuration += Time.deltaTime;
 
-        var progress = _currentTimeDuration / _recoloringDuration;
+        _currentTimeDurationResizing += Time.deltaTime;
+
+        var progress = _currentTimeDurationResizing / _resizingDuration;
+        var currentSize = Vector3.Lerp(_startSize, _objectScale, progress);
+        _renderer.transform.localScale = currentSize;
+
+        if (!(_currentTimeDurationResizing >= _resizingDuration)) return;
+        _currentTimeDurationResizing = 0f;
+        _isWaitResizing = !_isWaitResizing;
+    }
+
+    private void GenerateNextScale()
+    {
+        (_objectScale, _startSize) = (_startSize, _objectScale);
+    }
+    
+    private void RecolorObject()
+    {
+        if (_isWaitRecoloring)
+        {
+            _currentTimeDetentionRecoloring += Time.deltaTime;
+
+            if (!(_currentTimeDetentionRecoloring >= _detention)) return;
+            _currentTimeDetentionRecoloring = 0f;
+            GenerateNextColor();
+            _isWaitRecoloring = !_isWaitRecoloring;
+
+            return;
+        }
+
+        _currentTimeDurationRecoloring += Time.deltaTime;
+
+        var progress = _currentTimeDurationRecoloring / _recoloringDuration;
         var currentColor = Color.Lerp(_startColor, _nextColor, progress);
         _renderer.material.color = currentColor;
 
-        if (_currentTimeDuration >= _recoloringDuration)
-        {
-            _currentTimeDuration = 0f;
-            _isWaitRecoloring = !_isWaitRecoloring;
-        }
+        if (!(_currentTimeDurationRecoloring >= _recoloringDuration)) return;
+        _currentTimeDurationRecoloring = 0f;
+        _isWaitRecoloring = !_isWaitRecoloring;
     }
 
     private void GenerateNextColor()
